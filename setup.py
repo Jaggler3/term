28d1fa1313e91c468c371248fad5354d907e0901
Jaggler3 <martin.protostar@gmail.com>
2020-08-17 23:18:39 -0400

    config

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Jaggler3/term'
 EMAIL = 'martin.protostar@gmail.com'
 AUTHOR = 'Martin Darazs'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.4'
+VERSION = '0.1.5'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
