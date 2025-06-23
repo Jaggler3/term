3984fa162ce86a57960a6d09544a48ec819838ea
Martin D. <martin.protostar@gmail.com>
2025-06-23 04:00:59 -0400

    get ready for .2 release

--- setup.py
+++ setup.py
@@ -4,12 +4,8 @@
 # Note: To use the 'upload' functionality of this file, you must:
 #   $ pipenv install twine --dev
 
-import io
 import os
-import sys
-from shutil import rmtree
-
-from setuptools import find_packages, setup, Command
+from setuptools import find_packages, setup
 
 # Package meta-data.
 NAME = 'termbrowser'
@@ -18,11 +14,11 @@ URL = 'https://github.com/Jaggler3/term'
 EMAIL = 'martin.protostar@gmail.com'
 AUTHOR = 'Martin Darazs'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.7b'
+VERSION = '0.2.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-	'requests', 'simpleeval', 'pyperclip'
+	'requests', 'simpleeval', 'pyperclip', 'art'
 ]
 
 EXTRAS = { }
@@ -34,69 +30,11 @@ EXTRAS = { }
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-	with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-		long_description = '\n' + f.read()
-except FileNotFoundError:
-	long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-	project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-	with open(os.path.join(here, project_slug, '__version__.py')) as f:
-		exec(f.read(), about)
-else:
-	about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-	"""Support setup.py upload."""
-
-	description = 'Build and publish the package.'
-	user_options = []
-
-	@staticmethod
-	def status(s):
-		"""Prints things in bold."""
-		print('\033[1m{0}\033[0m'.format(s))
-
-	def initialize_options(self):
-		pass
-
-	def finalize_options(self):
-		pass
-
-	def run(self):
-		try:
-			self.status('Removing previous builds…')
-			rmtree(os.path.join(here, 'dist'))
-		except OSError:
-			pass
-
-		self.status('Building Source and Wheel (universal) distribution…')
-		os.system(
-			'{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-		self.status('Uploading the package to PyPI via Twine…')
-		os.system('twine upload dist/*')
-
-		self.status('Pushing git tags…')
-		os.system('git tag v{0}'.format(about['__version__']))
-		os.system('git push --tags')
-
-		sys.exit()
-
-
 # Where the magic happens:
 setup(
 	name=NAME,
-	version=about['__version__'],
+	version=VERSION,
 	description=DESCRIPTION,
-	long_description=long_description,
-	long_description_content_type='text/markdown',
 	author=AUTHOR,
 	author_email=EMAIL,
 	python_requires=REQUIRES_PYTHON,
@@ -118,13 +56,9 @@ setup(
 		'Programming Language :: Python :: Implementation :: CPython',
 		'Programming Language :: Python :: Implementation :: PyPy'
 	],
-	# $ setup.py publish support.
-	cmdclass={
-		'upload': UploadCommand,
-	},
 	entry_points={
 		'console_scripts': [
-			'term=termbrowser.app.main:main'
+			'term=termbrowser.app:main'
 		],
 	},
 )
