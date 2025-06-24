066c85da98389b669e176bf4a9813f933d6d853a
Martin D. <martin.protostar@gmail.com>
2025-06-23 20:39:29 -0400

    package modules

---
+++ pikobrowser/__init__.py
@@ -0,0 +1,7 @@
+"""
+Piko Browser - A terminal-based web browser.
+"""
+
+from piko.app import main
+
+__all__ = ["main"] 
---
+++ pikobrowser/__main__.py
@@ -0,0 +1,8 @@
+"""
+Entry point for running pikobrowser as a module.
+"""
+
+from piko.app import main
+
+if __name__ == "__main__":
+    main() 
